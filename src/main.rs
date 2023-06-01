26bf627760d1948dea94466457dc373c9703ed3e
George Dewhirst <dewhirst.george@gmail.com>
2023-06-01 22:34:02 +1000

    added exit command

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
-use std::io::{self, Write};
+use std::io;
+use std::io::Write;
+use std::process;
 
 fn main() -> io::Result<()> {
-    
+
     loop {
         print!("db > ");
         io::stdout().flush().expect("Unable to flush stdout");
         let mut buffer = String::new();
         io::stdin().read_line(&mut buffer)?;
-        let output = buffer
+        let command = buffer
             .strip_suffix("\r\n")
             .or(buffer.strip_suffix("\n"))
             .unwrap_or(&buffer);
-        println!("Was this your command: \"{}\"?", output);
+
+        if command == ".exit" {
+            process::exit(0x00)
+        }
+
+        println!("unrecognised command: \"{}\"", command);
     }
 }
