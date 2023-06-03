f243e8b6149809f40b71561771c07b5ef2c7d780
George Dewhirst <dewhirst.george@gmail.com>
2023-06-04 01:19:59 +1000

    multiple files - very cool

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::io;
 use std::io::Write;
 
+pub mod meta_commands;
+
 fn main() -> io::Result<()> {
 
-    println!(r#"Welcome to geoql! a rusty version of sqlite,
+    println!(r#"Welcome to gql! a rusty version of sqlite
     type .help to see a list of commands"#);
 
     loop {
@@ -19,7 +21,7 @@ fn main() -> io::Result<()> {
             .unwrap_or(&buffer);
 
         if user_input.starts_with(".") {
-            let _ = match exec_meta_command(MetaCommand::from(user_input)) {
+            let _ = match meta_commands::exec_meta_command(meta_commands::MetaCommand::from(user_input)) {
                 Ok(res) => println!("{}", res),
                 Err(err) => println!("Error: {}", err)
             };
@@ -29,61 +31,7 @@ fn main() -> io::Result<()> {
     }
 }
 
-fn exec_meta_command(command: MetaCommand) -> Result<&'static str, &'static str> {
-    match command {
-        MetaCommand::Exit(_) => {
-            std::process::exit(0)
-        },
-        MetaCommand::Help(_) => {
-            let message = r#"
-            available commands:
-            .exit            => quits the program
-            .help            => displays this message
-            .open <FILENAME> => opens a new database from FILENAME
-            .save <FILENAME> => saves current in memory database to FILENAME
-            .tables          => lists available tables
-            "#;
-            Ok(message)
-        },
-        MetaCommand::Open(_) => {
-            Err("Not Implemented")
-        },
-        MetaCommand::Save(_) => {
-            Err("Not Implemented")
-        },
-        MetaCommand::Tables(_) => {
-            Err("Not Implemented")
-        },
-        MetaCommand::Unknown(_) => {
-            Err("Command not recognised")
-        }
-    }
-}
 
-enum MetaCommand {
-    Exit(Vec<String>),
-    Help(Vec<String>),
-    Open(Vec<String>),
-    Save(Vec<String>),
-    Tables(Vec<String>),
-    Unknown(Vec<String>)
-}
-
-impl MetaCommand {
-    fn from(user_input: &str) -> MetaCommand {
-        let words: Vec<String> = user_input.split(" ").map(|s| s.to_string()).collect();
-        let command_str: &str = words[0].as_str();
-        let args: Vec<String> = words[1..].to_vec();
-        match command_str {
-            ".exit" => MetaCommand::Exit(args),
-            ".help" => MetaCommand::Help(args),
-            ".open" => MetaCommand::Open(args),
-            ".save" => MetaCommand::Save(args),
-            ".tables" => MetaCommand::Tables(args),
-            _ => MetaCommand::Unknown(args)
-        }
-    }
-}
 
 // fn exec_sql_command(command: &str) {
 //     // TO DO
---
+++ src/meta_commands.rs
@@ -0,0 +1,56 @@
+pub enum MetaCommand {
+    Exit(Vec<String>),
+    Help(Vec<String>),
+    Open(Vec<String>),
+    Save(Vec<String>),
+    Tables(Vec<String>),
+    Unknown(Vec<String>)
+}
+
+// matches strings input by the user with values in the enum MetaCommand
+impl MetaCommand {
+    pub fn from(user_input: &str) -> MetaCommand {
+        let words: Vec<String> = user_input.split(" ").map(|s| s.to_string()).collect();
+        let command_str: &str = words[0].as_str();
+        let args: Vec<String> = words[1..].to_vec();
+        match command_str {
+            ".exit" => MetaCommand::Exit(args),
+            ".help" => MetaCommand::Help(args),
+            ".open" => MetaCommand::Open(args),
+            ".save" => MetaCommand::Save(args),
+            ".tables" => MetaCommand::Tables(args),
+            _ => MetaCommand::Unknown(args)
+        }
+    }
+}
+
+pub fn exec_meta_command(command: MetaCommand) -> Result<&'static str, &'static str> {
+    match command {
+        MetaCommand::Exit(_) => {
+            std::process::exit(0)
+        },
+        MetaCommand::Help(_) => {
+            let message = r#"
+            available commands:
+            .exit            => quits the program
+            .help            => displays this message
+            .open <FILENAME> => opens a new database from FILENAME
+            .save <FILENAME> => saves current in memory database to FILENAME
+            .tables          => lists available tables
+            "#;
+            Ok(message)
+        },
+        MetaCommand::Open(_) => {
+            Err("Not Implemented")
+        },
+        MetaCommand::Save(_) => {
+            Err("Not Implemented")
+        },
+        MetaCommand::Tables(_) => {
+            Err("Not Implemented")
+        },
+        MetaCommand::Unknown(_) => {
+            Err("Command not recognised")
+        }
+    }
+}
